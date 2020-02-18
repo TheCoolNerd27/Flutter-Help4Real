7649991e28d8a82cde191c9806121b6b09d54929
TheCoolNerd27 <rohan.ashra27@gmail.com>
2020-02-18 18:50:32 +0530

    Added Feature to crop image before post

--- lib/PostScreen.dart
+++ lib/PostScreen.dart
@@ -6,8 +6,12 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:io';
 import 'package:path/path.dart' as path;
+import 'package:image_cropper/image_cropper.dart';
 
-
+/*
+TODO:
+1.Add Configuration for iOS for imagePicker
+ */
 
 class MyPostPage extends StatelessWidget {
    String title='Post';
@@ -35,16 +39,34 @@ class MyPostPage extends StatelessWidget {
    void upload() async {
      //pick image   use ImageSource.camera for accessing camera.
      File image = await ImagePicker.pickImage(source: ImageSource.gallery);
-
+     File croppedFile = await ImageCropper.cropImage(
+         sourcePath: image.path,
+         aspectRatioPresets: [
+           CropAspectRatioPreset.square,
+           CropAspectRatioPreset.ratio3x2,
+           CropAspectRatioPreset.original,
+           CropAspectRatioPreset.ratio4x3,
+           CropAspectRatioPreset.ratio16x9
+         ],
+         androidUiSettings: AndroidUiSettings(
+             toolbarTitle: 'Cropper',
+             toolbarColor: Colors.blue,
+             toolbarWidgetColor: Colors.white,
+             initAspectRatio: CropAspectRatioPreset.original,
+             lockAspectRatio: false),
+         iosUiSettings: IOSUiSettings(
+           minimumAspectRatio: 1.0,
+         )
+     );
      //basename() function will give you the filename
-     String fileName =path.basename(image.path);
+     String fileName =path.basename(croppedFile.path);
      var now=new DateTime.now();
      //passing your path with the filename to Firebase Storage Reference
      StorageReference reference =
      FirebaseStorage().ref().child("posts/$now/$fileName");
 
      //upload the file to Firebase Storage
-     StorageUploadTask uploadTask = reference.putFile(image);
+     StorageUploadTask uploadTask = reference.putFile(croppedFile);
 
      //Snapshot of the uploading task
      StorageTaskSnapshot storageTaskSnapshot = await uploadTask.onComplete;
@@ -53,3 +75,19 @@ class MyPostPage extends StatelessWidget {
    }
 
 }
+class PostForm extends StatefulWidget {
+  @override
+  _PostFormState createState() => _PostFormState();
+}
+
+class _PostFormState extends State<PostForm> {
+    final _formkey = GlobalKey<FormState>();
+    File _image;
+    String caption,tags;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
